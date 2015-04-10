ec5b60dd763e249e84d1d6a12eaa62c7764d1d21
kenz <kenzmitnick@gmail.com>
2015-04-10 16:58:13 +0000

    Internationalization des pages "maquettes".

---
+++ code source/assets/js/translate_ajouteoperation.js
@@ -0,0 +1,23 @@
+// load I18N bundles
+$(document).ready(function () {
+    loadBundles($.i18n.browserLang());
+    // configure language combo box
+    $('#lang').change(function () {
+        var selection = $('#lang option:selected').val();
+        loadBundles(selection !== 'browser' ? selection : $.i18n.browserLang());
+    });
+});
+
+function loadBundles(lang) {
+    $.i18n.properties({
+        name: 'Messages',
+        path: 'assets/bundle/',
+        mode: 'both',
+        language: lang,
+        callback: function () {
+            //$("#msg_welcome").text($.i18n.prop('msg_welcome'));
+            $("#msg_welcome").text(msg_welcome);
+            $("#btn_quit").text(btn_quit);
+        }
+    });
+}
